23e345944d926caf3b8a1eb47c9014d6fc2b4da7
reshmi-02 <reshmihash123@gmail.com>
2023-12-13 10:16:58 +0530

    Addnew

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div>
+                    <h4 className='header-head'>
+                        Menu
+                    </h4>
+
+                    <p className='header-para'>
+                        <i> Unique wood fired pizzas or be tempted by our pasta, antipasto, focaccias, salads  and Bruschetta dishes.
+                            Relas in the friendly ambience as you enjoy the aromas of the
+                            wood ovens or sit al fesco and sook up the Mildura sunshines.</i>
+                    </p>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header 
